2e4ca729906b6fbc522f0e907efb6f5e1a2273b1
Jangho614 <gimjangho197@gmail.com>
2024-09-12 21:02:30 +0900

    Orange

--- app/src/main/java/com/example/a_say_it/MainActivity.java
+++ app/src/main/java/com/example/a_say_it/MainActivity.java
@@ -1,24 +1,61 @@
 package com.example.a_say_it;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 각 fragment 클래스의 변수 선언
+    PronounceFragment Fragment1;
+    WordsFragment Fragment2;
+    MyWordsFragment Fragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Fragment1 = new PronounceFragment();
+        Fragment2 = new WordsFragment();
+        Fragment3 = new MyWordsFragment();
+
+        // 하단바 메뉴 변수 선언
+        BottomNavigationView bottomMenu = findViewById(R.id.bottom_menu);
+
+        // 기본 시작 화면 지정
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, Fragment1).commit();
+        bottomMenu.setSelectedItemId(R.id.menu_pronounce);
+        bottomMenu.setItemIconTintList(null);
+
+        bottomMenu.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.menu_pronounce:
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.container, Fragment1).commit();
+                                return true;
+                            case R.id.menu_randWords:
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.container, Fragment2).commit();
+                                return true;
+                            case R.id.menu_myWords:
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.container, Fragment3).commit();
+                                return true;
+                        }
+                        return false;
+                    }
+                }
+        );
     }
 }
